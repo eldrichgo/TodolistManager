db3fe3bd4f69713dbc70b97ee26876edf341094b
eldrichgo <eldrich_go@dlsu.edu.ph>
2024-07-11 01:52:21 +0800

    Fixxed resolver

--- todolistManager/Server/graph/generated.go
+++ todolistManager/Server/graph/generated.go
@@ -54,8 +54,7 @@ type ComplexityRoot struct {
 	}
 
 	Query struct {
-		TaskByID func(childComplexity int, id string) int
-		Tasks    func(childComplexity int) int
+		Tasks func(childComplexity int) int
 	}
 
 	Task struct {
@@ -72,7 +71,6 @@ type MutationResolver interface {
 }
 type QueryResolver interface {
 	Tasks(ctx context.Context) ([]*model.Task, error)
-	TaskByID(ctx context.Context, id string) (*model.Task, error)
 }
 
 type executableSchema struct {
@@ -129,18 +127,6 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.Mutation.UpdateTask(childComplexity, args["id"].(string), args["title"].(*string), args["status"].(*string)), true
-
-	case "Query.taskById":
-		if e.complexity.Query.TaskByID == nil {
-			break
-		}
-
-		args, err := ec.field_Query_taskById_args(context.TODO(), rawArgs)
-		if err != nil {
-			return 0, false
-		}
-
-		return e.complexity.Query.TaskByID(childComplexity, args["id"].(string)), true
 
 	case "Query.tasks":
 		if e.complexity.Query.Tasks == nil {
@@ -377,21 +363,6 @@ func (ec *executionContext) field_Query___type_args(ctx context.Context, rawArgs
 		}
 	}
 	args["name"] = arg0
-	return args, nil
-}
-
-func (ec *executionContext) field_Query_taskById_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
-	var err error
-	args := map[string]interface{}{}
-	var arg0 string
-	if tmp, ok := rawArgs["id"]; ok {
-		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("id"))
-		arg0, err = ec.unmarshalNID2string(ctx, tmp)
-		if err != nil {
-			return nil, err
-		}
-	}
-	args["id"] = arg0
 	return args, nil
 }
 
@@ -653,66 +624,6 @@ func (ec *executionContext) fieldContext_Query_tasks(_ context.Context, field gr
 			}
 			return nil, fmt.Errorf("no field named %q was found under type Task", field.Name)
 		},
-	}
-	return fc, nil
-}
-
-func (ec *executionContext) _Query_taskById(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
-	fc, err := ec.fieldContext_Query_taskById(ctx, field)
-	if err != nil {
-		return graphql.Null
-	}
-	ctx = graphql.WithFieldContext(ctx, fc)
-	defer func() {
-		if r := recover(); r != nil {
-			ec.Error(ctx, ec.Recover(ctx, r))
-			ret = graphql.Null
-		}
-	}()
-	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
-		ctx = rctx // use context from middleware stack in children
-		return ec.resolvers.Query().TaskByID(rctx, fc.Args["id"].(string))
-	})
-	if err != nil {
-		ec.Error(ctx, err)
-		return graphql.Null
-	}
-	if resTmp == nil {
-		return graphql.Null
-	}
-	res := resTmp.(*model.Task)
-	fc.Result = res
-	return ec.marshalOTask2ᚖserverᚋgraphᚋmodelᚐTask(ctx, field.Selections, res)
-}
-
-func (ec *executionContext) fieldContext_Query_taskById(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
-	fc = &graphql.FieldContext{
-		Object:     "Query",
-		Field:      field,
-		IsMethod:   true,
-		IsResolver: true,
-		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
-			switch field.Name {
-			case "id":
-				return ec.fieldContext_Task_id(ctx, field)
-			case "title":
-				return ec.fieldContext_Task_title(ctx, field)
-			case "status":
-				return ec.fieldContext_Task_status(ctx, field)
-			}
-			return nil, fmt.Errorf("no field named %q was found under type Task", field.Name)
-		},
-	}
-	defer func() {
-		if r := recover(); r != nil {
-			err = ec.Recover(ctx, r)
-			ec.Error(ctx, err)
-		}
-	}()
-	ctx = graphql.WithFieldContext(ctx, fc)
-	if fc.Args, err = ec.field_Query_taskById_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
-		ec.Error(ctx, err)
-		return fc, err
 	}
 	return fc, nil
 }
@@ -2845,25 +2756,6 @@ func (ec *executionContext) _Query(ctx context.Context, sel ast.SelectionSet) gr
 				if res == graphql.Null {
 					atomic.AddUint32(&fs.Invalids, 1)
 				}
-				return res
-			}
-
-			rrm := func(ctx context.Context) graphql.Marshaler {
-				return ec.OperationContext.RootResolverMiddleware(ctx,
-					func(ctx context.Context) graphql.Marshaler { return innerFunc(ctx, out) })
-			}
-
-			out.Concurrently(i, func(ctx context.Context) graphql.Marshaler { return rrm(innerCtx) })
-		case "taskById":
-			field := field
-
-			innerFunc := func(ctx context.Context, _ *graphql.FieldSet) (res graphql.Marshaler) {
-				defer func() {
-					if r := recover(); r != nil {
-						ec.Error(ctx, ec.Recover(ctx, r))
-					}
-				}()
-				res = ec._Query_taskById(ctx, field)
 				return res
 			}
 
--- todolistManager/Server/graph/resolver.go
+++ todolistManager/Server/graph/resolver.go
@@ -14,13 +14,14 @@ func NewResolver(taskService *service.TaskService) *Resolver {
 	}
 }
 
-func (r *Resolver) Query() QueryResolver {
-	return &queryResolver{r}
-}
+//Implemented by gqlgen automatically when you run the command "go run github.com/99designs/gqlgen" in the Server/graph directory in schema.resolvers.go???
+// func (r *Resolver) Query() QueryResolver {
+// 	return &queryResolver{r}
+// }
 
-func (r *Resolver) Mutation() MutationResolver {
-	return &mutationResolver{r}
-}
+// func (r *Resolver) Mutation() MutationResolver {
+// 	return &mutationResolver{r}
+// }
 
-type queryResolver struct{ *Resolver }
-type mutationResolver struct{ *Resolver }
+// type queryResolver struct{ *Resolver }
+// type mutationResolver struct{ *Resolver }
